e60740f0fc3c6151c5e8b69f1cb78e08b72d620d
neuronflow <florian.kofler@tum.de>
2023-08-29 15:29:23 +0200

    expose
    Signed-off-by: neuronflow <florian.kofler@tum.de>

--- auxiliary/__init__.py
+++ auxiliary/__init__.py
@@ -3,3 +3,7 @@ from .nifti import read_nifti, write_nifti
 from .path import turbopath, name_extractor
 
 from .runscript import ScriptRunner
+
+from .normalization.percentile_normalizer import PercentileNormalizer
+from .normalization.windowing_normalizer import WindowingNormalizer
+from .normalization.functional import *
