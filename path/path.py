391f10371c16a9dd67cbc8e1196ef459a1f23350
neuronflow <florian.kofler@tum.de>
2023-10-04 12:01:36 +0200

    path
    Signed-off-by: neuronflow <florian.kofler@tum.de>

---
+++ path/path.py
@@ -0,0 +1,31 @@
+from path import Path
+import os
+
+
+def turbopath(input_path: str):
+    """
+    Normalize and convert a given input path to a normalized Path object.
+
+    Args:
+        input_path (str): The input path to be processed.
+
+    Returns:
+        Path: A Path object representing the normalized absolute path.
+    """
+    # Normalize the input path by converting it to an absolute path
+    # and then normalizing any directory separators
+    normalized_path = os.path.normpath(
+        os.path.abspath(
+            input_path,
+        )
+    )
+    # Create a Path object from the normalized absolute path
+    turbo_path = Path(normalized_path)
+    return turbo_path
+
+
+def name_extractor(input_path: str):
+    input_path = turbopath(input_path)
+    file_name = input_path.name
+    parts = file_name.split(".")
+    return parts[0]
